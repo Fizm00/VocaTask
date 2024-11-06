d0a8c8e3e23e51fc6a2de4f785b257841a11711d
Firza Himawan <firzahimawan.2022@student.uny.ac.id>
2024-11-07 01:43:16 +0700

    adding new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
+import Login from './pages/Login'; // Halaman login
+import SignUp from './pages/SignUp'; // Halaman signup
+import Task from './pages/Task'; // Halaman task
+import EditProfile from './pages/EditProfile';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} /> {/* Halaman Login */}
+        <Route path="/signup" element={<SignUp />} /> {/* Halaman SignUp */}
+        <Route path="/task" element={<Task />} /> {/* Halaman Task */}
+        <Route path='/editprofile' element={<EditProfile />} />
       </Routes>
     </Router>
   );
